95a0a3cd5609715fd255e1b52f2f051b9e2f64ce
Jelena Andjelic <jelena.andjelic.gr@gmail.com>
2022-12-12 12:14:14 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo quam qui
+        veniam magnam architecto, ducimus ullam non tenetur recusandae! Rem ea
+        eius animi deleniti dolorem aliquam quasi inventore voluptatum optio
+        esse! Nulla sunt eaque similique ea eveniet optio perferendis et?
+      </p>
+    </section>
+  );
+};
+
+export default About;
